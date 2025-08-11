fc88fd8b78707e49d04219a0e3a1103c0fa10894
Greg pc-montmorillonm <gregoire.herbin@free.fr>
2025-08-11 20:54:39 +0200

    Dov env ajout index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,14 @@
 <?php
 
+use Symfony\Component\Dotenv\Dotenv;
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+// Charger les variables d'environnement seulement si le fichier .env existe
+if (file_exists(__DIR__.'/../.env')) {
+    (new Dotenv())->loadEnv(__DIR__.'/../.env');
+}
 
+// Retourner la fonction anonyme qui crée le Kernel en fonction du contexte
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
